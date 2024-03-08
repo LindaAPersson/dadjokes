19878dbe66205a7f06311022efc2d08afd073817
Linda Persson <lindapersson-@live.se>
2024-03-08 09:00:00 +0000

    <fest>[admin]installed summernote

--- dadjokes/urls.py
+++ dadjokes/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'), name='home_page'),
     path('', include('jokes.urls'), name='jokes_page'),
 #    path('about/', include('about.urls'), name='about_page'),
--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Joke, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Joke)
+@admin.register(Joke)
+class JokeAdmin(SummernoteModelAdmin):
+
+    list_display = ('id', 'creator', 'status')
+    search_fields = ['id']
+    list_filter = ('status',)
+    summernote_fields = ('joke_text',)
+
+
 admin.site.register(Comment)

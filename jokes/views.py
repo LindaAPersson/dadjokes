fa4bd67ccc9c259adb111060f3b8a01a1e0aa2b1
Linda Persson <lindapersson-@live.se>
2024-03-08 14:54:21 +0000

    <bugfix>[jokes]removed likes code

--- jokes/views.py
+++ jokes/views.py
@@ -9,12 +9,5 @@ class JokeList(generic.ListView):
     template_name = "jokes/jokes.html"
     paginate_by = 1
     
-#def JokeLike(request, pk):
-#    post = get_object_or_404(JokeList, id=request.POST.get('like_buton'))
-#    if post.likes.filter(id=request.user.id).exists():
-#        post.likes.remove(request.user)
-#    else:
-#        post.likes.add(request.user)
-#
-#    return HttpResponseRedirect(reverse('jokes_page', args=[str(pk)]))     
+  
 

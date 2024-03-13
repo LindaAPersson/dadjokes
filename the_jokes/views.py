a69e5d491ebf76145b80c8a9654283291105fe4d
Linda Persson <lindapersson-@live.se>
2024-03-13 13:35:15 +0000

    <feat>[joke_detail]connected the_jokes and jokes_detail

--- the_jokes/views.py
+++ the_jokes/views.py
@@ -7,5 +7,16 @@ from .models import Joke, Comment
 class joke_list(generic.ListView):
 
     queryset = Joke.objects.filter(status=1)
-    template_name = "jokes/jokes.html"
+    template_name = "the_jokes/the_jokes.html"
     paginate_by = 1
+
+
+def joke_detail(request, title):
+    queryset = Joke.objects.filter(status=1)
+    jokes = get_object_or_404(queryset, title=title)
+
+    return render(
+        request,
+        "the_jokes/jokes_detail.html",
+        {"jokes": jokes},
+    )

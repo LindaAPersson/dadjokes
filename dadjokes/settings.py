5f5ff6dbcc0955656fcf39815c54d7e2e732f994
Linda Persson <lindapersson-@live.se>
2024-03-13 10:42:50 +0000

    <bug>[comment]i cant fix the bug, so im creating a new app to start over

--- dadjokes/settings.py
+++ dadjokes/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lindaapersson-dadjokes-u2rd1wa2lfe.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-lindaapersson-dadjokes-u2rd1wa2lfe.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition

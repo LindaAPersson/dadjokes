1e69c6f6dbc576db7ac0a350f8718f5003803139
Linda Persson <lindapersson-@live.se>
2024-03-16 20:43:47 +0000

    <feat>[creator image]placeholder img, and user img uppload

---
+++ staticfiles/js/jokes.js
@@ -0,0 +1,21 @@
+const deleteModal = document.getElementById("modal2");
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+/**
+ * Geting the deleteing comments model confirm
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, preventScrolling=true);
+});
+
+
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let jokeId = e.target.getAttribute("joke_id");
+    deleteConfirm.href = `delete_joke/${jokeId}`;
+    deleteModal.show();
+  });
+}

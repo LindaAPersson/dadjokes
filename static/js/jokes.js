bcc299df64b86813ac87a8c282d9002ef6a84625
Linda Persson <lindapersson-@live.se>
2024-03-19 14:21:11 +0000

    <feat>[next button]changed the next button]

--- static/js/jokes.js
+++ static/js/jokes.js
@@ -36,3 +36,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems,);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
+});

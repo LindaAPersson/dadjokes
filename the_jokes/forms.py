80daa9ed7d1a81113227b9984d18986784937419
Linda Persson <lindapersson-@live.se>
2024-03-25 12:54:24 +0000

    <chore>[forms]uppdate with docstrings

--- the_jokes/forms.py
+++ the_jokes/forms.py
@@ -1,43 +1,58 @@
 from .models import Comment, Joke, Category, Rating
 from django import forms
 
-# add jokes
-class JokeForm(forms.ModelForm):
 
+class JokeForm(forms.ModelForm):
+    """
+    A form for adding jokes
+    """
     class Meta:
         model = Joke
         fields = ('joke_text', 'creator_image', 'category',)
         
     
-# Edit jokes
 class EditJokeForm(forms.ModelForm):
-    
+    """
+    A form for edit jokes
+    """ 
     class Meta:
         model = Joke
         fields = ('joke_text', 'creator_image',)    
 
-# Like jokes
+
 class LikesForm(forms.ModelForm):
+    """
+    A form for like jokes
+    """ 
 
     class Meta:
         model = Joke
         fields = ('likes',)
 
-# comment jokes
+
 class CommentForm(forms.ModelForm):
+    """
+    A form for comment on jokes
+    """ 
     class Meta:
         model = Comment
         fields = ('comment_text',)
 
-# rate jokes
+
 class RateForm(forms.ModelForm):
+    """
+    A form for rating jokes
+    """ 
     class Meta:
         model = Rating
         fields = ('rating',)
 
-# age approved
-class AgeForm(forms.ModelForm):
 
+class AgeForm(forms.ModelForm):
+    """
+    A form for age approvel. The users can filter the jokes
+    depending on age approved. 
+    """
     class Meta:
         model = Joke
         fields = ('age_approved',)
